6ec4472b1dc9e69b0af39e77c9ac617cddbb8411
@ddovgal <Danylo_Dovhal@epam.com>
2025-08-28 14:57:54 +0200

    Update CatalogItemListPagedEndpoint.cs

--- src/PublicApi/CatalogItemEndpoints/CatalogItemListPagedEndpoint.cs
+++ src/PublicApi/CatalogItemEndpoints/CatalogItemListPagedEndpoint.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.eShopWeb.PublicApi.CatalogItemEndpoints;
 
@@ -14,7 +15,7 @@ namespace Microsoft.eShopWeb.PublicApi.CatalogItemEndpoints;
 /// List Catalog Items (paged)
 /// </summary>
 public class CatalogItemListPagedEndpoint(IRepository<CatalogItem> itemRepository, IUriComposer uriComposer,
-        AutoMapper.IMapper mapper)
+        AutoMapper.IMapper mapper, ILogger<CatalogItemListPagedEndpoint> _logger)
     : Endpoint<ListPagedCatalogItemRequest, ListPagedCatalogItemResponse>
 {
     public override void Configure()
@@ -29,6 +30,7 @@ public class CatalogItemListPagedEndpoint(IRepository<CatalogItem> itemRepositor
     public override async Task<ListPagedCatalogItemResponse> ExecuteAsync(ListPagedCatalogItemRequest request, CancellationToken ct)
     {
         await Task.Delay(1000, ct);
+        _logger.LogInformation("TEST test");
 
         var response = new ListPagedCatalogItemResponse(request.CorrelationId());
 
